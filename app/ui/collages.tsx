feed473045e2fd902ddba0a9bc7d81e70d1109a7
Enevald <enevaldbravo80@gmail.com>
2024-05-21 13:02:32 +0300

    first comit

--- app/ui/collages.tsx
+++ app/ui/collages.tsx
@@ -1,4 +1,4 @@
-"use client"; 
+"use client";
 
 import "../globals.css";
 import React, { useState } from 'react';
@@ -9,58 +9,61 @@ interface CollageProps {
   name: string;
 }
 
-const Collage = ({ images, name }) => {
+const Collage: React.FC<CollageProps> = ({ images, name }) => {
   const [showComponent, setShowComponent] = useState(false);
 
   const handleClick = () => {
     setShowComponent(!showComponent);
   };
 
-  const handleIconClick = (e) => {
+  const handleIconClick = (e: React.MouseEvent<HTMLSpanElement>) => {
     e.stopPropagation(); // Prevent the click event from bubbling up to the parent div
     setShowComponent(false); // Hide the component
   };
 
   return (
-  <div>
-   <h1>{name}</h1>
-    <div onClick={handleClick} id="pc" style={{ cursor: 'pointer' }} className="collage grid grid-cols-2 grid-rows-4 gap-3">
-      {/* First row */}
-      <div className="col-span-2 row-span-2">
-        <img src={images[0]} alt="Image 1" className="w-full h-full object-cover" />
+    <div>
+      <h1>{name}</h1>
+      <div onClick={handleClick} id="pc" style={{ cursor: 'pointer' }} className="collage grid grid-cols-2 grid-rows-4 gap-3">
+        {/* First row */}
+        <div className="col-span-2 row-span-2">
+          <img src={images[0]} alt="Image 1" className="w-full h-full object-cover" />
+        </div>
+        <div className="col-span-1 row-span-1">
+          <img src={images[1]} alt="Image 2" className="w-full h-full object-cover" />
+        </div>
+        <div className="col-span-1 row-span-1">
+          <img src={images[2]} alt="Image 3" className="w-full h-full object-cover" />
+        </div>
+        {/* Second row */}
+        <div className="col-span-1 row-span-1">
+          <img src={images[3]} alt="Image 4" className="w-full h-full object-cover" />
+        </div>
+        <div className="col-span-1 row-span-1">
+          <img src={images[4]} alt="Image 5" className="w-full h-full object-cover" />
+        </div>
       </div>
-      <div className="col-span-1 row-span-1">
-        <img src={images[1]} alt="Image 2" className="w-full h-full object-cover" />
+      <div onClick={handleClick} id="tablet" style={{ cursor: 'pointer' }} className="collage grid grid-cols-2 grid-rows-3 gap-3">
+        {/* First row */}
+        <div className="col-span-2 row-span-2">
+          <img src={images[0]} alt="Image 1" className="w-full h-full object-cover" />
+        </div>
+        <div className="col-span-1 row-span-1">
+          <img src={images[1]} alt="Image 2" className="w-full h-full object-cover" />
+        </div>
+        {/* Second row */}
+        <div className="col-span-1 row-span-1">
+          <img src={images[4]} alt="Image 5" className="w-full h-full object-cover" />
+        </div>
       </div>
-      <div className="col-span-1 row-span-1">
-        <img src={images[2]} alt="Image 3" className="w-full h-full object-cover" />
-      </div>
-      {/* Second row */}
-      <div className="col-span-1 row-span-1">
-        <img src={images[3]} alt="Image 4" className="w-full h-full object-cover" />
-      </div>
-      <div className="col-span-1 row-span-1">
-        <img src={images[4]} alt="Image 5" className="w-full h-full object-cover" />
-      </div>
-    </div>
-    <div onClick={handleClick} id="tablet" style={{ cursor: 'pointer' }} className="collage grid grid-cols-2 grid-rows-3 gap-3">
-      {/* First row */}
-      <div className="col-span-2 row-span-2">
-        <img src={images[0]} alt="Image 1" className="w-full h-full object-cover" />
-      </div>
-      <div className="col-span-1 row-span-1">
-        <img src={images[1]} alt="Image 2" className="w-full h-full object-cover" />
-      </div>
-      {/* Second row */}
-      <div className="col-span-1 row-span-1">
-        <img src={images[4]} alt="Image 5" className="w-full h-full object-cover" />
-      </div>
-    </div>
-    {showComponent && <div>
-      <span className="togg" onClick={handleIconClick} style={{ cursor: 'pointer' }}>
+      {showComponent && (
+        <div>
+          <span className="togg" onClick={handleIconClick} style={{ cursor: 'pointer' }}>
             Back {name}
-      </span>
-      <Grid images={images} /></div>}
+          </span>
+          <Grid images={images} />
+        </div>
+      )}
     </div>
   );
 };
